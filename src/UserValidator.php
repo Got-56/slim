75417ad760940e8ea9aac40c5a57b78cc0f27651
Qwerty <lkdot@proton.me>
2024-10-05 21:10:53 +0500

    added src

---
+++ src/UserValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class UserValidator
+{
+    public function validate(array $user): array
+    {
+        $errors =[];
+        if (mb_strlen($user['nickname']) < 4) {
+            $errors['nickname'] = "Nickname must be grater than 4 characters";
+        }
+        return $errors;
+    }
+}
