1a56f2be4d310b9579a31108e65562aab1e75cf5
Qwerty <lkdot@proton.me>
2024-10-05 21:05:15 +0500

    Added user creation. Added named routes

--- public/index.php
+++ public/index.php
@@ -13,20 +13,71 @@ $container->set('renderer', function () {
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
-$app->get('/users', function ($request, $response) use ($users){
-    $term = $request->getQueryParam('term');
-    $filteredCourses = array_filter($users, function ($user) use ($term) {
-        return str_contains($user, $term);
-    });
-    $params = ['users' => $filteredCourses];
-    return $this->get('renderer')->render($response, "users/index.phtml", $params);
-});
+$router = $app->getRouteCollector()->getRouteParser();
 
+$app->get('/', function ($request, $response) use ($router) {
+    $createUserUrl = $router->urlFor('user.create');
+    $userListUrl = $router->urlFor('user.list');
+
+    return $response->write("<a href='{$createUserUrl}'>Create User</a>");
+})->setName('home');
+
+$app->post('/users', function ($request, $response) {
+    $userData = $request->getParsedBodyParam('user');
+    $id = uniqid();
+    $repo = __DIR__ . '/UserRepository.json'; // repository of users
+    if (file_exists($repo)) { // read existing users
+        $users = json_decode(file_get_contents($repo), true); // decode JSON
+    } else {
+        $users = [];
+    }
+    $users[] = [
+        'nickname' => $userData['nickname'],
+        'email' => $userData['email'],
+        'id' => $id
+    ];
+    file_put_contents($repo, json_encode($users, JSON_PRETTY_PRINT));
+    return $response->withStatus(302)->withHeader('Location', '/users');
+})->setName('user.store');
+
+$app->get('/users/new', function ($request, $response) use ($router) {
+    $params = ['userData' => [], 'router' => $router];
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+})->setName('user.create');
+
+
+$app->get('/users', function ($request, $response) use ($router) {
+    $term = $request->getQueryParam('term');
+
+
+    $repo = __DIR__ . '/UserRepository.json';
+    if (file_exists($repo)) {
+        $users = json_decode(file_get_contents($repo), true);
+    } else {
+        $users = [];
+    }
+
+
+    if ($term) {
+        $filteredUsers = array_filter($users, function ($user) use ($term) {
+            return str_contains($user['nickname'], $term);
+        });
+    } else {
+        $filteredUsers = $users;
+    }
+
+    $params = [
+        'users' => $filteredUsers,
+        'term' => $term,
+        'router' => $router
+    ];
+
+    return $this->get('renderer')->render($response, "users/index.phtml", $params);
+})->setName('user.list');
 
 $app->get('/users/{id:.+}', function ($request, $response, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user.show');
 
 $app->run();
