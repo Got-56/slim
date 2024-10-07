47510f4581d344f8f5cd507fe09df52e17abfa6d
Qwerty <lkdot@proton.me>
2024-10-07 10:56:30 +0500

    add UserValidator

--- src/UserValidator.php
+++ src/UserValidator.php
@@ -6,9 +6,11 @@ class UserValidator
 {
     public function validate(array $user): array
     {
-        $errors =[];
-        if (mb_strlen($user['nickname']) < 4) {
-            $errors['nickname'] = "Nickname must be grater than 4 characters";
+        $errors = [];
+        if (empty($user['nickname'])) {
+            $errors['nickname'] = 'Nickname cannot be empty';
+        } elseif (mb_strlen($user['nickname']) < 4) {
+            $errors['nickname'] = 'Nickname must be at least 4 characters long';
         }
         return $errors;
     }
