7a3a166688acbf3942e0a0e65bc74a888285268f
Qwerty <lkdot@proton.me>
2024-10-08 11:25:20 +0500

    flash message added

--- public/index.php
+++ public/index.php
@@ -7,9 +7,15 @@ use DI\Container;
 use App\UserValidator;
 use App\UserRepository;
 
+session_start();
+
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
 });
 
 $app = AppFactory::createFromContainer($container);
@@ -19,9 +25,13 @@ $app->addErrorMiddleware(true, true, true);
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) use ($router) {
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+        'flash' => $messages ?? [],
+    ];
 
     return $response->withRedirect($router->urlFor('index.users'), 302);
-});
+})->setName('home');
 
 $repo = new \App\UserRepository(__DIR__ . '/UsersRepository.json');
 
@@ -31,12 +41,16 @@ $app->post('/users', function ($request, $response) use ($repo, $router) {
     $errors = $validator->validate($user);
     if (count($errors) === 0) {
         $repo->save($user);
+
+        $this->get('flash')->addMessage('success', 'User was added successfully');
+
         return $response->withRedirect($router->urlFor('index.users'),302);
     }
     $params = [
         'user' => $user,
         'errors' => $errors
     ];
+
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('create.user');
 
@@ -46,16 +60,28 @@ $app->get('/users/new', function ($request, $response) use ($router){
         'errors' => [],
         'router' => $router
     ];
+
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('new.user');
 
 $app->get('/users', function ($request, $response) use ($repo, $router){
+    $messages = $this->get('flash')->getMessages();
+
     $term = $request->getQueryParam('term','');
+
     $users = $repo->getAllUsers();
+
     $filteredUsers = array_filter($users, function ($user) use ($term) {
         return empty($term) || str_contains($user['nickname'], $term);
     });
-    $params = ['users' => $filteredUsers, 'term' => $term, 'router' => $router];
+
+    $params = [
+        'users' => $filteredUsers,
+        'term' => $term,
+        'flash' => $messages,
+        'router' => $router
+    ];
+
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('index.users');
 
@@ -63,10 +89,13 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($repo) {
     $id = $args['id'];
     $users = $repo->getAllUsers();
     $user = array_filter($users, fn($user)=> $user['id'] === $id);
+
     if (empty($user)) {
         return $response->write('User not found')->withStatus(404);
     }
+
     $params = ['user' => array_shift($user)];
+
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('show.user');
 
