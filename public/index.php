86b584e8767ee2234af28ff60d971d2be5a441e1
Qwerty <lkdot@proton.me>
2024-10-10 20:04:00 +0500

    added user editing

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
 use App\UserValidator;
 use App\UserRepository;
@@ -21,11 +22,13 @@ $container->set('flash', function () {
 $app = AppFactory::createFromContainer($container);
 
 $app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
 
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) use ($router) {
     $messages = $this->get('flash')->getMessages();
+
     $params = [
         'flash' => $messages ?? [],
     ];
@@ -39,22 +42,24 @@ $app->post('/users', function ($request, $response) use ($repo, $router) {
     $validator = new UserValidator();
     $user = $request->getParsedBodyParam('user');
     $errors = $validator->validate($user);
+
     if (count($errors) === 0) {
         $repo->save($user);
-
         $this->get('flash')->addMessage('success', 'User was added successfully');
-
         return $response->withRedirect($router->urlFor('index.users'),302);
     }
+
     $params = [
         'user' => $user,
-        'errors' => $errors
+        'errors' => $errors,
+        'router' => $router
     ];
 
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('create.user');
 
 $app->get('/users/new', function ($request, $response) use ($router){
+
     $params = [
         'user' => ['nickname' => '', 'email' => ''],
         'errors' => [],
@@ -66,11 +71,8 @@ $app->get('/users/new', function ($request, $response) use ($router){
 
 $app->get('/users', function ($request, $response) use ($repo, $router){
     $messages = $this->get('flash')->getMessages();
-
     $term = $request->getQueryParam('term','');
-
     $users = $repo->getAllUsers();
-
     $filteredUsers = array_filter($users, function ($user) use ($term) {
         return empty($term) || str_contains($user['nickname'], $term);
     });
@@ -85,18 +87,71 @@ $app->get('/users', function ($request, $response) use ($repo, $router){
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('index.users');
 
-$app->get('/users/{id}', function ($request, $response, $args) use ($repo) {
+$app->get('/users/{id}', function ($request, $response, $args) use ($repo, $router) {
     $id = $args['id'];
-    $users = $repo->getAllUsers();
-    $user = array_filter($users, fn($user)=> $user['id'] === $id);
+    $user = $repo->find($id);
 
-    if (empty($user)) {
+    if (!$user) {
         return $response->write('User not found')->withStatus(404);
     }
 
-    $params = ['user' => array_shift($user)];
+    $params = [
+        'user' => $user,
+        'router' => $router
+    ];
 
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('show.user');
+
+$app->get('/users/{id}/edit', function ($request, $response, $args) use ($repo, $router) {
+    $messages = $this->get('flash')->getMessages();
+    $id = $args['id'];
+    $user = $repo->find($id);
+
+    if (!$user) {
+        return $response->write('User not found')->withStatus(404);
+    }
+
+    $params = [
+        'user' => $user,
+        'errors' => [],
+        'router' => $router,
+        'flash' => $messages
+    ];
+
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('user.edit');
+
+$app->patch('/users/{id}', function ($request, $response, $args) use ($repo, $router) {
+    $id = $args['id'];
+    $user = $repo->find($id);
+
+    if (!$user) {
+        return $response->write('User not found')->withStatus(404);
+    }
+
+    $userData = $request->getParsedBodyParam('user');
+    $validator = new UserValidator();
+    $errors = $validator->validate($userData);
+
+    if (count($errors) === 0) {
+        $user['nickname'] = $userData['nickname'];
+        $user['email'] = $userData['email'];
+
+        $repo->save($user);
+
+        $this->get('flash')->addMessage('success', "User was updated successfully");
+        $url = $router->urlFor('user.edit', ['id' => $user['id']]);
+        return $response->withRedirect($url);
+    }
+
+    $params = [
+        'user' => $userData,
+        'errors' => $errors
+    ];
+
+    $response = $response->withStatus(422);
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('user.update');
 
 $app->run();
