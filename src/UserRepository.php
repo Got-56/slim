e47fd0191f590885ffd0763e232ef59ab94cabc0
Qwerty <lkdot@proton.me>
2024-10-10 20:05:34 +0500

    minor updates

--- src/UserRepository.php
+++ src/UserRepository.php
@@ -1,20 +1,35 @@
 <?php
 
 namespace App;
-class UserRepository {
+
+class UserRepository
+{
     private $repoFile;
 
-    public function __construct(string $repoFile) {
+    public function __construct(string $repoFile)
+    {
         $this->repoFile = $repoFile;
     }
 
     public function save(array $user): void
     {
         $users = $this->getAllUsers();
-        $id = uniqid();
-        $user['id'] = $id;
-        $users[$id] = $user;
-        file_put_contents($this->repoFile, json_encode($users, JSON_PRETTY_PRINT));
+        $found = false;
+
+        foreach ($users as &$existingUser) {
+            if ($existingUser['id'] === $user['id']) {
+                $existingUser = $user;
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found) {
+            $user['id'] = $user['id'] ?? uniqid();
+            $users[] = $user;
+        }
+
+        file_put_contents($this->repoFile, json_encode($users, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     }
 
     public function getAllUsers(): array
@@ -23,5 +38,16 @@ class UserRepository {
             return json_decode(file_get_contents($this->repoFile), true) ?? [];
         }
         return [];
+    }
+
+    public function find(string $id): ?array
+    {
+        $users = $this->getAllUsers();
+        foreach ($users as $user) {
+            if ($user['id'] === $id) {
+                return $user;
+            }
+        }
+        return null;
     }
 }
