5025532fbd8ae5f4348287ebe3fd125a5fc16ae4
Qwerty <lkdot@proton.me>
2024-10-12 16:27:24 +0500

    mail checking removed

--- src/UserValidator.php
+++ src/UserValidator.php
@@ -7,17 +7,8 @@ class UserValidator
     public function validate(array $user): array
     {
         $errors = [];
-
-        if (empty($user['nickname'])) {
-            $errors['nickname'] = 'Nickname cannot be empty';
-        } elseif (mb_strlen($user['nickname']) < 4) {
-            $errors['nickname'] = 'Nickname must be at least 4 characters long';
-        }
-
-        if (empty($user['email'])) {
-            $errors['email'] = 'Email cannot be empty';
-        } elseif (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['email'] = 'Email is not valid';
+        if (mb_strlen($user['nickname']) < 4) {
+            $errors['nickname'] = "Nickname must be grater than 4 characters";
         }
 
         return $errors;
