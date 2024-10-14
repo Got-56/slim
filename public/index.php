e36236d89749f5a7f486afd32a05bbbd2afc4b9f
Qwerty <lkdot@proton.me>
2024-10-14 10:57:28 +0500

    added handlers for cars

--- public/index.php
+++ public/index.php
@@ -2,12 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Car;
+use App\CarRepository;
+use App\CarValidator;
 use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
 use App\UserValidator;
 
 session_start();
+
+const ADMIN_EMAIL = 'admin@proton.me';
 
 function getUsers($request) {
     return json_decode($request->getCookieParam('users') ?? '', true);
@@ -26,6 +31,16 @@ $container->set('flash', function () {
     return new \Slim\Flash\Messages();
 });
 
+$container->set(\PDO::class, function () {
+    $conn = new \PDO('sqlite:database.sqlite');
+    $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    return $conn;
+});
+
+$initFilePath = implode('/', [dirname(__DIR__), 'init.sql']);
+$initSql = file_get_contents($initFilePath);
+$container->get(\PDO::class)->exec($initSql);
+
 $app = AppFactory::createFromContainer($container);
 
 $router = $app->getRouteCollector()->getRouteParser();
@@ -34,6 +49,10 @@ $app->addErrorMiddleware(true, true, true);
 $app->add(MethodOverrideMiddleware::class);
 
 $app->get('/', function ($request, $response) use ($router) {
+    if (isset($_SESSION['isAdmin'])) {
+        return $response->withRedirect($router->urlFor('users.index'));
+    }
+
     $messages = $this->get('flash')->getMessages();
     $params = [
         'email' => '',
@@ -43,7 +62,33 @@ $app->get('/', function ($request, $response) use ($router) {
     return $this->get('renderer')->render($response, 'home.phtml', $params);
 })->setName('home');
 
+$app->post('/login', function ($request, $response) use ($router) {
+    $email = $request->getParsedBodyParam('email');
+
+    if ($email === ADMIN_EMAIL) {
+        $_SESSION['isAdmin'] = true;
+
+        return $response->withRedirect($router->urlFor('users.index'));
+    }
+
+    $this->get('flash')->addMessage('error', 'Access Denied!');
+
+    return $response->withRedirect($router->urlFor('home'));
+});
+
+$app->delete('/logout', function ($request, $response) use ($router) {
+    session_destroy();
+
+    return $response->withRedirect($router->urlFor('home'));
+});
+
 $app->get('/users', function ($request, $response) use ($router){
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $term = $request->getQueryParam('term') ?? '';
     $users = getUsers($request) ?? [];
     $usersList = isset($term) ? filterUsersByName($users, $term) : $users;
@@ -86,6 +131,11 @@ $app->post('/users', function ($request, $response) use ($router) {
 })->setName('users.store');
 
 $app->get('/users/new', function ($request, $response) use ($router){
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
 
     $params = [
         'userData' => [],
@@ -96,6 +146,12 @@ $app->get('/users/new', function ($request, $response) use ($router){
 })->setName('users.create');
 
 $app->get('/users/{id}', function ($request, $response, $args) use ($router) {
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $id = $args['id'];
     $users = getUsers($request);
 
@@ -115,6 +171,12 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($router) {
 })->setName('users.show');
 
 $app->get('/users/{id}/edit', function ($request, $response, $args) use ($router) {
+    if (!isset($_SESSION['isAdmin'])) {
+        $this->get('flash')->addMessage('error', 'Access Denied! Please login!');
+
+        return $response->withRedirect($router->urlFor('home'));
+    }
+
     $messages = $this->get('flash')->getMessages();
     $id = $args['id'];
     $users = getUsers($request);
@@ -171,6 +233,115 @@ $app->delete('/users/{id}', function ($request, $response, $args) use ($router) 
 
     return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
         ->withRedirect($router->urlFor('users.index'));
+});
+
+$app->get('/cars', function ($request, $response) {
+    $carRepository = $this->get(CarRepository::class);
+    $cars = $carRepository->getEntities();
+
+    $messages = $this->get('flash')->getMessages();
+
+    $params = [
+        'cars' => $cars,
+        'flash' => $messages
+    ];
+
+    return $this->get('renderer')->render($response, 'cars/index.phtml', $params);
+})->setName('cars.index');
+
+$app->post('/cars', function ($request, $response) use ($router) {
+    $carRepository = $this->get(CarRepository::class);
+    $carData = $request->getParsedBodyParam('car');
+
+    $validator = new CarValidator();
+    $errors = $validator->validate($carData);
+
+    if (count($errors) === 0) {
+        $car = Car::fromArray([$carData['make'], $carData['model']]);
+        $carRepository->save($car);
+        $this->get('flash')->addMessage('success', 'Car was added successfully');
+        return $response->withRedirect($router->urlFor('cars.index'));
+    }
+
+    $params = [
+        'car' => $carData,
+        'errors' => $errors
+    ];
+
+    return $this->get('renderer')->render($response->withStatus(422), 'cars/new.phtml', $params);
+})->setName('cars.store');
+
+$app->get('/cars/new', function ($request, $response) {
+    $params = [
+        'car' => new Car(),
+        'errors' => []
+    ];
+
+    return $this->get('renderer')->render($response, 'cars/new.phtml', $params);
+})->setName('cars.create');
+
+$app->get('/cars/{id}', function ($request, $response, $args) {
+    $carRepository = $this->get(CarRepository::class);
+    $id = $args['id'];
+    $car = $carRepository->find($id);
+
+    if (is_null($car)) {
+        return $response->write('Page not found')->withStatus(404);
+    }
+
+    $messages = $this->get('flash')->getMessages();
+
+    $params = [
+        'car' => $car,
+        'flash' => $messages
+    ];
+
+    return $this->get('renderer')->render($response, 'cars/show.phtml', $params);
+})->setName('cars.show');
+
+$app->get('/cars/{id}/edit', function ($request, $response, $args) {
+    $carRepository = $this->get(CarRepository::class);
+    $messages = $this->get('flash')->getMessages();
+    $id = $args['id'];
+    $car = $carRepository->find($id);
+
+    $params = [
+        'car' => $car,
+        'errors' => [],
+        'flash' => $messages
+    ];
+
+    return $this->get('renderer')->render($response, 'cars/edit.phtml', $params);
+})->setName('cars.edit');
+
+$app->patch('/cars/{id}', function ($request, $response, $args) use ($router) {
+    $carRepository = $this->get(CarRepository::class);
+    $id = $args['id'];
+
+    $car = $carRepository->find($id);
+
+    if (is_null($car)) {
+        return $response->write('Page not found')->withStatus(404);
+    }
+
+    $carData = $request->getParsedBodyParam('car');
+    $validator = new CarValidator();
+    $errors = $validator->validate($carData);
+
+    if (count($errors) === 0) {
+        $car->setMake($carData['make']);
+        $car->setModel($carData['model']);
+        $carRepository->save($car);
+        $this->get('flash')->addMessage('success', "Car was updated successfully");
+        return $response->withRedirect($router->urlFor('cars.show', $args));
+    }
+
+    $params = [
+        'car' => $car,
+        'errors' => $errors
+    ];
+
+    return $this->get('renderer')->render($response->withStatus(422), 'cars/edit.phtml', $params);
 });
 
 $app->run();
